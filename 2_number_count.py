a5e1d045efedc87d4b8a1aeb0e7fbff5f239a23c
demmanou <demmanou@cisco.com>
2022-11-15 08:48:28 +0100

    number count challenge

---
+++ 2_number_count.py
@@ -0,0 +1,32 @@
+import unittest
+from typing import List
+
+
+# Given a list of numbers as an input, 
+# return a dictionary with the frequency 
+# each number is seen at in the list
+
+
+class Solution:
+    def number_count(self, numbers: List[int]) -> list:
+        pass
+
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ([5, 5, 10, 2, 1, 0, -5, 1],
+        {5: 2, 10: 1, 2: 1, 1: 2, 0: 1, -5: 1})
+    ]
+
+    test_functions = [
+        Solution().number_count
+    ]
+
+    def test_word_count(self):
+        for input_, expected in self.test_cases:
+            for func in self.test_functions:
+                assert func(input_) == expected, 'Failed!'
+
+
+if __name__ == "__main__":
+    unittest.main()
