87bd993aa38a55b2308ecb871ac00d1dc8b75979
demmanou <demmanou@cisco.com>
2022-11-15 09:01:56 +0100

    challenge 03

---
+++ 3_actor_sort.py
@@ -0,0 +1,32 @@
+import unittest
+from typing import List, Tuple
+
+
+# Given a list of actor names in (first, last name) pairs,
+# return a list with the same pairs, sorted by descending 
+# alphabetical order of the actors' last names
+
+
+class Solution:
+    def actor_sort(self, actors: list) -> list:
+        pass
+
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ([('John', 'Wayne'), ('Charlton', 'Heston'), ('Sean', 'Connery'), ('Robert', 'Redford'), ('Alain', 'Delon')],
+            [('John', 'Wayne'), ('Robert', 'Redford'), ('Charlton', 'Heston'), ('Alain', 'Delon'), ('Sean', 'Connery')])
+    ]
+
+    test_functions = [
+        Solution().actor_sort
+    ]
+
+    def test_funcs(self):
+        for input_, expected in self.test_cases:
+            for func in self.test_functions:
+                assert func(input_) == expected, 'Failed!'
+
+
+if __name__ == "__main__":
+    unittest.main()
